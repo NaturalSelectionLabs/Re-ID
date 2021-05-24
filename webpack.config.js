f042e2cf39e3b8f94fe8fff90f4cacdb2df40f65
DIYgod <i@diygod.me>
2021-05-24 22:48:52 +0800

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: {
+        background: './src/background/index.js',
+        'content-script': './src/content-script/index.js',
+        options: './src/options/index.js',
+        popup: './src/popup/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/',
+    },
+
+    resolve: {
+        extensions: ['.js', '.less'],
+    },
+
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                  appendTsSuffixTo: [/\.vue$/],
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    require('autoprefixer'),
+                                    require('cssnano')({
+                                        preset: 'default',
+                                    }),
+                                ],
+                            },
+                        }
+                    },
+                    'less-loader'
+                ],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    'limit': 40000
+                }
+            },
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets',
+                    to: '',
+                },
+            ],
+        }),
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify(require('./src/assets/manifest.json').version)
+        }),
+    ],
+};
