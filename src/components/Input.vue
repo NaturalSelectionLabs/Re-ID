f54c6a33d5590a1ba93e47eaf91bd805edd9e497
azuic <zuic10a@gmail.com>
2021-05-28 14:36:42 +0800

    chore: update config file

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -59,38 +59,37 @@ export default class Input extends Vue {
 </script>
 
 <style lang="postcss">
-
 @layer components {
-.input {
-    @apply bg-gray-bg focus:bg-white focus:border focus:border-gray-outline rounded text-left font-regular placeholder-gray-text placeholder-opacity-30 text-black;
-}
+    .input {
+        @apply bg-gray-bg focus:bg-white focus:border focus:border-gray-outline rounded text-left font-regular placeholder-gray-text placeholder-opacity-30 text-black;
+    }
 
+    .input-popup {
+        @apply w-55 h-9 rounded px-16 py-10 text-xs; //font-12px
+    }
 
-.input-popup {
-    @apply w-55 h-9 rounded px-16 py-10 text-xs; //font-12px
-}
+    .input-popup-text {
+        @apply h-9;
+    }
+    .input-popup-textarea {
+        @apply h-23;
+    }
 
-.input-popup-text {
-    @apply h-9;
-}
-.input-popup-textarea {
-    @apply h-23;
-}
+    .input-options {
+        @apply w-180 h-18 rounded px-32 py-20 text-2xl; //font-24px
+    }
 
-.input-options {
-    @apply w-180 h-18 rounded px-32 py-20 text-2xl; //font-24px
-}
+    .input-options-text {
+        @apply h-18;
+    }
 
-.input-options-text {
-    @apply h-18;
-}
+    .input-options-textarea {
+        @apply h-45;
+    }
 
-.input-options-textarea {
-    @apply h-45;
-}
-
-/*  raw rss3 codeblock input private key */
-.input-options-compact {
-    @apply w-186 h-18;
+    /*  raw rss3 codeblock input private key */
+    .input-options-compact {
+        @apply w-186 h-18;
+    }
 }
 </style>
