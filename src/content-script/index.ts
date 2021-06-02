edd7f78fa8fb8a02667f4e965597db1efcb38edd
Candinya <dev@candinya.com>
2021-06-03 00:43:53 +0800

    update(content-script): use curly brackets to pack codes

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -5,71 +5,79 @@ import { TwitterButtonSync, TwitterButtonFollow } from './components';
 observe('[data-testid="toolBar"] div', (ele: Element): void => {
     ele.insertAdjacentHTML('beforeend', TwitterButtonSync);
 
-    function updateSyncStatusClass(enabled: boolean) {
-        const twiBtnSyncUut = document.getElementById(
-            'reid-sync-active-status',
-        );
-        if (twiBtnSyncUut !== null) {
-            if (enabled) {
-                twiBtnSyncUut.classList.add('active');
-            } else {
-                twiBtnSyncUut.classList.remove('active');
+    {
+        // Listen events
+        function updateSyncStatusClass(enabled: boolean) {
+            const twiBtnSyncUut = document.getElementById(
+                'reid-sync-active-status',
+            );
+            if (twiBtnSyncUut !== null) {
+                if (enabled) {
+                    twiBtnSyncUut.classList.add('active');
+                } else {
+                    twiBtnSyncUut.classList.remove('active');
+                }
             }
         }
-    }
 
-    function setRSS3Sync(enabled: boolean) {
-        updateSyncStatusClass(enabled);
-        chrome.storage.sync.set({ 'reid-twitter-sync-enabled': enabled });
-    }
-    chrome.storage.sync.get(['reid-twitter-sync-enabled'], (result) => {
-        const enabled = result['reid-twitter-sync-enabled'];
-        if (typeof enabled !== 'undefined') {
+        function setRSS3Sync(enabled: boolean) {
             updateSyncStatusClass(enabled);
+            chrome.storage.sync.set({ 'reid-twitter-sync-enabled': enabled });
         }
-    });
-    const twiBtnSyncEnaUut = document.getElementById(
-        'reid-sync-button-activate',
-    );
-    if (twiBtnSyncEnaUut !== null) {
-        twiBtnSyncEnaUut.addEventListener('click', () => {
-            setRSS3Sync(true);
+        chrome.storage.sync.get(['reid-twitter-sync-enabled'], (result) => {
+            const enabled = result['reid-twitter-sync-enabled'];
+            if (typeof enabled !== 'undefined') {
+                updateSyncStatusClass(enabled);
+            }
         });
-    }
-    const twiBtnSyncDeUut = document.getElementById(
-        'reid-sync-button-deactivate',
-    );
-    if (twiBtnSyncDeUut !== null) {
-        twiBtnSyncDeUut.addEventListener('click', () => {
-            setRSS3Sync(false);
-        });
+        const twiBtnSyncEnaUut = document.getElementById(
+            'reid-sync-button-activate',
+        );
+        if (twiBtnSyncEnaUut !== null) {
+            twiBtnSyncEnaUut.addEventListener('click', () => {
+                setRSS3Sync(true);
+            });
+        }
+        const twiBtnSyncDeUut = document.getElementById(
+            'reid-sync-button-deactivate',
+        );
+        if (twiBtnSyncDeUut !== null) {
+            twiBtnSyncDeUut.addEventListener('click', () => {
+                setRSS3Sync(false);
+            });
+        }
     }
 });
 
 observe('[data-testid="placementTracking"]', (ele: Element): void => {
     ele.insertAdjacentHTML('beforebegin', TwitterButtonFollow);
 
-    let followStatus = false;
-    function updateFollowStatusClass(fostat: boolean) {
-        const twiBtnFoUut = document.getElementById('reid-follow');
-        if (twiBtnFoUut !== null) {
-            if (fostat) {
-                twiBtnFoUut.classList.add('active');
-            } else {
-                twiBtnFoUut.classList.remove('active');
+    {
+        // Listen events
+        let followStatus = false;
+        function updateFollowStatusClass(fostat: boolean) {
+            const twiBtnFoUut = document.getElementById('reid-follow');
+            if (twiBtnFoUut !== null) {
+                if (fostat) {
+                    twiBtnFoUut.classList.add('active');
+                } else {
+                    twiBtnFoUut.classList.remove('active');
+                }
             }
         }
-    }
-    function toggleFollowStatus() {
-        followStatus = !followStatus;
+        function toggleFollowStatus() {
+            followStatus = !followStatus;
+            updateFollowStatusClass(followStatus);
+        }
         updateFollowStatusClass(followStatus);
-    }
-    updateFollowStatusClass(followStatus);
-    const twiBtnFoToUut = document.getElementById('reid-follow-button-toggle');
-    if (twiBtnFoToUut !== null) {
-        twiBtnFoToUut.addEventListener('click', () => {
-            toggleFollowStatus();
-        });
+        const twiBtnFoToUut = document.getElementById(
+            'reid-follow-button-toggle',
+        );
+        if (twiBtnFoToUut !== null) {
+            twiBtnFoToUut.addEventListener('click', () => {
+                toggleFollowStatus();
+            });
+        }
     }
 });
 
