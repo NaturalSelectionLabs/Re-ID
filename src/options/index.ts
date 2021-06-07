96ec8044b49f9d7a43764bba6a4ef9201573e3ee
DIYgod <i@diygod.me>
2021-06-08 03:36:10 +0800

    refactor: router.ts

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,82 +1,8 @@
 import { createApp } from 'vue';
-import * as VueRouter from 'vue-router';
 import '@/assets/main.css';
 
 import App from './App.vue';
-
-import Entry from './pages/Entry.vue';
-import About from './pages/About.vue';
-import Address from './pages/Address.vue';
-import Advanced from './pages/Advanced.vue';
-import Home from './pages/Home.vue';
-import Invite from './pages/Invite.vue';
-import Login from './pages/Login.vue';
-import Pending from './pages/Pending.vue';
-import PrivateKey from './pages/PrivateKey.vue';
-import Profile from './pages/Profile.vue';
-import Settings from './pages/Settings.vue';
-import Setup from './pages/Setup.vue';
-import Start from './pages/Start.vue';
-
-const routes = [
-    {
-        path: '/',
-        component: Entry,
-    },
-    {
-        path: '/about',
-        component: About,
-    },
-    {
-        path: '/address',
-        component: Address,
-    },
-    {
-        path: '/advanced',
-        component: Advanced,
-    },
-    {
-        path: '/home',
-        component: Home,
-    },
-    {
-        path: '/invite',
-        component: Invite,
-    },
-    {
-        path: '/login',
-        component: Login,
-    },
-    {
-        path: '/pending',
-        component: Pending,
-    },
-    {
-        path: '/privateKey',
-        component: PrivateKey,
-    },
-    {
-        path: '/profile',
-        component: Profile,
-    },
-    {
-        path: '/settings',
-        component: Settings,
-    },
-    {
-        path: '/setup',
-        component: Setup,
-    },
-    {
-        path: '/start',
-        component: Start,
-    },
-];
-
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes,
-});
+import router from './router';
 
 const app = createApp(App);
 
