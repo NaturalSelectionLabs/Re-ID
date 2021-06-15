47e536c883ba943f5e19feceb857428c082a2d6b
DIYgod <i@diygod.me>
2021-06-16 00:54:57 +0800

    feat: start profile and congrates

--- src/options/views/Start/Profile.vue
+++ src/options/views/Start/Profile.vue
@@ -1,10 +1,18 @@
-<template>Profile</template>
+<template>
+    <p class="flex justify-between">
+        <Button buttonStyle="primary" buttonSize="xl" @click="save()">Save and continue</Button>
+        <Button buttonStyle="secondary" buttonSize="xl" @click="$router.push('/start/congrats')">Skip for now</Button>
+    </p>
+</template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Button from '@/components/Button.vue';
 
 @Options({
-    components: {},
+    components: {
+        Button,
+    },
 })
 export default class StartProfile extends Vue {}
 </script>
