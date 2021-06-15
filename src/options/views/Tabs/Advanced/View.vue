6b7abb14e2db5c8fb7db1a1a0626adec31f558ad
Candinya <dev@candinya.com>
2021-06-16 01:27:10 +0800

    add: RSS3 raw persona

--- src/options/views/Tabs/Advanced/View.vue
+++ src/options/views/Tabs/Advanced/View.vue
@@ -1,11 +1,19 @@
 <template>
-    <textarea disabled> </textarea>
+    <textarea disabled v-model="rawData" />
 </template>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import RSS3 from '@/common/rss3';
 
 @Options({})
-export default class TabsAdvancedView extends Vue {}
+export default class TabsAdvancedView extends Vue {
+    rawData: string = '';
+
+    async mounted() {
+        const rss3 = await RSS3.get();
+        this.rawData = await rss3.persona.raw();
+    }
+}
 </script>
 <style lang="postcss" scoped>
 @layer components {
